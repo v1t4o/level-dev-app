76599adc96f1c776ae522c8492ee342e22b7e915
Victor de Oliveira <vitor.oliveira.marinho@gmail.com>
2023-03-02 19:04:51 -0300

    Helper para status da locação

---
+++ app/helpers/rental_helper.rb
@@ -0,0 +1,12 @@
+module RentalHelper
+  STATUS_COLORS = {
+    scheduled: 'primary',
+    ongoing: 'warning',
+    finalized: 'success'
+  }.freeze
+
+  def status(status)
+    color_class = STATUS_COLORS.fetch(status&.to_sym, 'primary')
+    content_tag :span, t(status, default: 'Status não existe'), class: "badge bg-#{color_class}"
+  end
+end
---
+++ spec/helpers/rental_helper_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+describe RentalHelper do
+  describe '#status' do
+    it 'should render blue when scheduled' do
+      rental = build(:rental, status: :scheduled)
+      result = helper.status(rental.status)
+      expect(result).to eq('<span class="badge bg-primary">agendada</span>')
+    end
+
+    it 'should render yellow when ongoing' do
+      rental = build(:rental, status: :ongoing)
+      result = helper.status(rental.status)
+      expect(result).to eq('<span class="badge bg-warning">em andamento</span>')
+    end
+
+    it 'should render green when finalized' do
+      rental = build(:rental, status: :finalized)
+      result = helper.status(rental.status)
+      expect(result).to eq('<span class="badge bg-success">finalizada</span>')
+    end
+
+    it 'should render blue when status not found' do
+      result = helper.status('default')
+      expect(result).to eq('<span class="badge bg-primary">Status não existe</span>')
+    end
+
+    it 'should render blue when status not found' do
+      result = helper.status(nil)
+      expect(result).to eq('<span class="badge bg-primary">Status não existe</span>')
+    end
+  end
+end
